468015de4cadc99773ae3ac29295c3d5d556c8b2
Nikolas Martin Nedbal <renshiakumuame@gmail.com>
2023-12-19 13:54:00 +0100

    LockSystem
    lockos systemos

---
+++ Assets/UnlockLevel.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UnlockLevel : MonoBehaviour
+{
+    private static List<Level> levels = new List<Level>();
+
+    [SerializeField] private List<string> sceneName = new List<string>();
+
+    private void Awake()
+    {
+        FillSceneList();
+        //DontDestroyOnLoad(this.gameObject); 
+    }
+
+    void FillSceneList()
+    {
+        foreach (var sceneInList in sceneName)
+        {   
+            Level newLevel = new Level(sceneInList);
+            levels.Add(newLevel);
+            Debug.Log("level prirazen " + newLevel.sceneName);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnlockFirstLevelAndMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadLevel(int i)
+    {
+        Level levelToLoad = levels[i];
+        Debug.Log("level " + i);
+
+        if (levelToLoad.isUnlocked == true)
+        {
+           SceneManager.LoadScene(levelToLoad.sceneName);
+           Debug.Log("level " + levelToLoad.sceneName);
+        }
+        else
+        {
+           Debug.Log("Locked");
+        }
+    }
+
+    void UnlockFirstLevelAndMenu()
+    {
+        Level menuLevel = levels[0];
+        Level firstLevel = levels[1];
+        menuLevel.Unlock();
+        firstLevel.Unlock();
+        Debug.Log("Levels Unlocked");
+    }
+
+    public void Victory(int nextLevelNumber)
+    {
+        Level nextLevel = levels[nextLevelNumber];
+        nextLevel.Unlock();
+    }
+
+    public void Defeat()
+    {
+        //Hodí to hráče do menu
+        SceneManager.LoadScene(0);
+    }
+
+    class Level
+    {
+        public bool isUnlocked;
+        public string sceneName;
+
+        public Level(string sceneName)
+        {
+            isUnlocked = false;
+            this.sceneName = sceneName;
+        }
+
+        public void Unlock()
+        {
+            isUnlocked = true;
+        }
+    }
+}
